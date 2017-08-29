ca4129bc8b1ec096e95ec5361fd89600b5aedb75
Ben Sarmiento <me@bensarmiento.com>
2017-08-30 00:21:32 +0800

    Move to src and apply proper namespace

--- src/Action/PingAction.php
+++ src/Action/PingAction.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Action;
+namespace Beebeelee\Paymaya\Action;
 
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Beebeelee\Paymaya;
 
 /**
  * The configuration provider for the App module

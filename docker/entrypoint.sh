10186225f7afb634afcad8024700da3e49b143a6
Ben Sarmiento <me@bensarmiento.com>
2017-08-30 01:06:09 +0800

    Fix docker config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eo pipefail
 
-if [ -z ${ARGO_DSN} ]; then
-    echo >&2 'error: you need to specify the ARGO_DSN environment variable '
+if [ -z ${BBL_PAYMAYA_DSN} ]; then
+    echo >&2 'error: you need to specify the BBL_PAYMAYA_DSN environment variable '
     exit 1
 fi
 

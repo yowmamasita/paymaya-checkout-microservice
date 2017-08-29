276d2e5cc0cbd6fc90b64e69aac4453ea1c94266
Ben Sarmiento <me@bensarmiento.com>
2017-08-30 00:21:46 +0800

    Create factory for doctrine entity manager

---
+++ src/Factory/EntityManagerFactory.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Ben Sarmiento <me@bensarmiento.com>
+ */
+
+namespace Beebeelee\Paymaya\Factory;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+use LogicException;
+use Psr\Container\ContainerInterface;
+
+final class EntityManagerFactory
+{
+    public function __invoke(ContainerInterface $container): EntityManagerInterface
+    {
+        $config = $container->get('config');
+
+        if (!isset($config['doctrine'])) {
+            throw new LogicException('Mandatory configuration [doctrine] missing');
+        }
+
+        $doctrineConfiguration = Setup::createXMLMetadataConfiguration(
+            $config['doctrine']['mapping_directories'],
+            $config['doctrine']['dev_mode'],
+            $config['doctrine']['proxy_directory']
+        );
+
+        return EntityManager::create($config['doctrine']['params'], $doctrineConfiguration);
+    }
+}
